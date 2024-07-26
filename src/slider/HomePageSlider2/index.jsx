04435a9a61ead3449be3747db4c331017107e9e1
Komal Prajapat <work.technotoil@gmail.com>
2024-07-26 19:58:06 +0530

    update

--- src/slider/HomePageSlider2/index.jsx
+++ src/slider/HomePageSlider2/index.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import "./index.css";
-import span from "../../components/ButtonForALL"; // Assuming this is correctly imported
 import { ImageUrl } from "../../API/baseUrl";
 import { Link } from "react-router-dom";
 
 const HoomepageSlider2 = ({ titleList }) => {
+  // Limit the number of items to 3
+  const limitedTitleList = titleList.slice(0, 3);
+
   return (
     <div className="HomepageSliderCon2 container-fluid">
       <div className="row">
-        {titleList.map((item, index) => (
+        {limitedTitleList.map((item, index) => (
           <div key={index} className="col-12 col-md-4">
             <div className="box box1">
-              <div className="box-content">
-                <p>{item.category_name}</p>
+              <div className="box-content d-flex">
+                <div>
+                  <p>{item.category_name}</p>
+                  <Link to="/product" className="DiscovernowBNT">
+                    Discover now
+                  </Link>
+                </div>
                 <img
                   src={`${ImageUrl}${item.files}`}
                   alt={item.category_name}
-                  style={{ width: "50px", height: "50px" }}
+                  style={{ width: "100px", height: "100px" }}
                 />
-                <Link to={"/product"} className="DiscovernowBNT" >Discover now</Link>
               </div>
             </div>
           </div>
