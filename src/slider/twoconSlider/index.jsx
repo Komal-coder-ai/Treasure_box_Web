9ead2247729b4d76dc38eb26bc021d34f8678617
Komal Prajapat <work.technotoil@gmail.com>
2024-07-30 16:52:36 +0530

    twoCon Slider

--- src/slider/twoconSlider/index.jsx
+++ src/slider/twoconSlider/index.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 
 const TwoConSlider = ({ titleList = [] }) => {
   // Filter to get items at index 4 and 5
-  const filteredTitleList = titleList.slice(3, 4);
+  const filteredTitleList = titleList.slice(3, 7);
 
   return (
     <Container className="HomepageSliderCon2">
@@ -15,9 +15,9 @@ const TwoConSlider = ({ titleList = [] }) => {
         {filteredTitleList.length > 0 ? (
           filteredTitleList.map((item, index) => (
             <Col key={index} xs={12} md={6} lg={6} className="mb-3">
-              <div className="box box1">
+              <div className="box box1 d-flex">
                 <Link to={`/product/${item.id}`} className="DiscovernowBNT">
-                  <div className="box-content d-flex flex-column align-items-center">
+                  <div className="box-content d-flex  align-items-center justify-content-evenly">
                     <div className="text-center mb-2">
                       <p>{item.category_name}</p>
                       <Link to={`/product/${item.id}`} className="DiscovernowBNT">
@@ -27,7 +27,7 @@ const TwoConSlider = ({ titleList = [] }) => {
                     <img
                       src={`${ImageUrl}${item.files}`}
                       alt={item.category_name}
-                      className="homepagesliderimage"
+                      className="homepagesliderimage mx-3"
                     />
                   </div>
                 </Link>
