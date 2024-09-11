6d382d1eeb13e21dd59f1e0983abb2027877651f
Komal-Prajapat <komalprajapat267@gmail.com>
2024-09-11 16:07:22 +0530

    filter button

--- src/components/newfilter/index.jsx
+++ src/components/newfilter/index.jsx
@@ -3,8 +3,9 @@ import CircleIcon from '@mui/icons-material/Circle';
 import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
 import CloseIcon from '@mui/icons-material/Close';
 import Slider from "@mui/material/Slider";
-import {  filterApi, postApiCall } from '../../API/baseUrl';
+import { filterApi, postApiCall } from '../../API/baseUrl';
 import "./index.css"
+import { InputAdornment, TextField } from '@mui/material';
 
 
 const SortByData = [
@@ -131,102 +132,101 @@ const Color = [
 ]
 
 
-const Newfilter = ({name,categoryvalueforapi,categoryList,categoryheading, setCategoryList, showfilter, setShowfilter ,setProductList, setSelectedlist, selectedlist, setSortval, sortval, setRanfgevaluefilter, ranfgevaluefilter }) => {
+const Newfilter = ({ name, categoryvalueforapi, categoryList, categoryheading, setCategoryList, showfilter, setShowfilter, setProductList, setSelectedlist, selectedlist, setSortval, sortval, setRanfgevaluefilter, ranfgevaluefilter }) => {
 
 
 	const [range, setRange] = React.useState([0, 1000]);
 	const [SortBy, setSortBy] = useState(SortByData);
 	const [namevalue, setNamevalue] = useState(name);
 	const [minPrice, setMinPrice] = useState(0);
-    const [maxPrice, setMaxPrice] = useState(1000);
+	const [maxPrice, setMaxPrice] = useState(1000);
 
 	const minDistance = 100
 	useEffect(() => {
-        setMinPrice(range[0]);
-        setMaxPrice(range[1]);
-    }, [range]);
+		setMinPrice(range[0]);
+		setMaxPrice(range[1]);
+	}, [range]);
 
 
 	const handleChange = async (event, newValue, activeThumb) => {
-        if (!Array.isArray(newValue)) {
-            return;
-        }
-        let rangeee;
-        if (activeThumb === 0) {
-            rangeee = [Math.min(newValue[0], range[1] - minDistance), range[1]];
-        } else {
-            rangeee = [range[0], Math.max(newValue[1], range[0] + minDistance)];
-        }
-        setRange([...rangeee]);
-        updatePriceFilter(rangeee);
-    };
+		if (!Array.isArray(newValue)) {
+			return;
+		}
+		let rangeee;
+		if (activeThumb === 0) {
+			rangeee = [Math.min(newValue[0], range[1] - minDistance), range[1]];
+		} else {
+			rangeee = [range[0], Math.max(newValue[1], range[0] + minDistance)];
+		}
+		setRange([...rangeee]);
+		updatePriceFilter(rangeee);
+	};
 
 
-  const handleMinPriceChange = (e) => {
-        const value = Math.max(0, Math.min(e.target.value, maxPrice - minDistance));
-        setMinPrice(value);
-        setRange([value, range[1]]);
-        updatePriceFilter([value, range[1]]);
-    };
+	const handleMinPriceChange = (e) => {
+		const value = Math.max(0, Math.min(e.target.value, maxPrice - minDistance));
+		setMinPrice(value);
+		setRange([value, range[1]]);
+		updatePriceFilter([value, range[1]]);
+	};
 
-    const handleMaxPriceChange = (e) => {
-        const value = Math.min(10000, Math.max(e.target.value, minPrice + minDistance));
-        setMaxPrice(value);
-        setRange([range[0], value]);
-        updatePriceFilter([range[0], value]);
-    };
+	const handleMaxPriceChange = (e) => {
+		const value = Math.min(10000, Math.max(e.target.value, minPrice + minDistance));
+		setMaxPrice(value);
+		setRange([range[0], value]);
+		updatePriceFilter([range[0], value]);
+	};
 
 
 
 
 	const updatePriceFilter = async (rangeee) => {
-        setRanfgevaluefilter(rangeee.join("-"));
+		setRanfgevaluefilter(rangeee.join("-"));
 
-        const data = selectedlist.findIndex((item) => item.type === "price");
-        const pricevalue = {
-            id: 49,
-            name: rangeee.join(" to "),
-            type: "price",
-        };
+		const data = selectedlist.findIndex((item) => item.type === "price");
+		const pricevalue = {
+			id: 49,
+			name: rangeee.join(" to "),
+			type: "price",
+		};
 
-        if (data === -1) {
-            setSelectedlist([...selectedlist, pricevalue]);
-        } else {
-            selectedlist.splice(data, 1);
-            setSelectedlist([...selectedlist, pricevalue]);
-        }
-        const apicategory = selectedlist.filter(selector => selector.type === "category");
-        try {
-            const result = await postApiCall(filterApi, {
-                search: "",
-                sort_by: sortval,
-                price: rangeee.join("-"),
-                category_name: apicategory.length ? apicategory.map(item => item.category_name).join(", ") : categoryvalueforapi,
-            });
-            if (result.data.status) {
-                setProductList(result.data.data);
-            }
-        } catch (error) {
-            console.log("error", error);
-        }
-    };
-	
-	const handlefilterapply = async (value ,type) => {
+		if (data === -1) {
+			setSelectedlist([...selectedlist, pricevalue]);
+		} else {
+			selectedlist.splice(data, 1);
+			setSelectedlist([...selectedlist, pricevalue]);
+		}
+		const apicategory = selectedlist.filter(selector => selector.type === "category");
+		try {
+			const result = await postApiCall(filterApi, {
+				search: "",
+				sort_by: sortval,
+				price: rangeee.join("-"),
+				category_name: apicategory.length ? apicategory.map(item => item.category_name).join(", ") : categoryvalueforapi,
+			});
+			if (result.data.status) {
+				setProductList(result.data.data);
+			}
+		} catch (error) {
+			console.log("error", error);
+		}
+	};
+
+	const handlefilterapply = async (value, type) => {
 		let filtercategoryval
 		const apicategory = selectedlist.filter(selector => selector.type === "category")
 		const categoryconcat = apicategory.map(item => item.category_name).join(", ")
-		if(type === "add"){
+		if (type === "add") {
 			if (categoryconcat.trim() !== '') {
 				const updatedvalue = `${categoryconcat},${value}`;
 				filtercategoryval = updatedvalue;
-			  } else {
-				filtercategoryval= value ;
-			  }
-		}if(type === "remove")
-		{
-				const updatedcategoryArray = apicategory?.filter(item => item.category_name !== value);
-				filtercategoryval = updatedcategoryArray.map(item => item.category_name).join(", ")
-			  };
+			} else {
+				filtercategoryval = value;
+			}
+		} if (type === "remove") {
+			const updatedcategoryArray = apicategory?.filter(item => item.category_name !== value);
+			filtercategoryval = updatedcategoryArray.map(item => item.category_name).join(", ")
+		};
 		try {
 			const result = await postApiCall(filterApi, {
 				search: "",
@@ -243,7 +243,7 @@ const Newfilter = ({name,categoryvalueforapi,categoryList,categoryheading, setCa
 		}
 	}
 
-	const ClearAll = async() => {
+	const ClearAll = async () => {
 		let array
 		if (array = SortBy) {
 			array.map(item => item.selected = false)
@@ -282,7 +282,7 @@ const Newfilter = ({name,categoryvalueforapi,categoryList,categoryheading, setCa
 		})
 		setCategoryList([...categoryList])
 		setSelectedlist(result => [...result, value]);
-		handlefilterapply(value.category_name , "add")
+		handlefilterapply(value.category_name, "add")
 	}
 
 	const onlistafterClick = async (value, array, index) => {
@@ -296,7 +296,7 @@ const Newfilter = ({name,categoryvalueforapi,categoryList,categoryheading, setCa
 
 		const categoryremovevalue = selectedlist.filter(item => item.category_name !== value.category_name)
 		setSelectedlist([...categoryremovevalue]);
-		handlefilterapply(value.category_name , "remove")
+		handlefilterapply(value.category_name, "remove")
 	}
 
 	const removelist = (item) => {
@@ -324,100 +324,116 @@ const Newfilter = ({name,categoryvalueforapi,categoryList,categoryheading, setCa
 		item.selected = false
 		setSelectedlist([...main])
 		setNamevalue("")
-		handlefilterapply(item.category_name , "remove")
+		handlefilterapply(item.category_name, "remove")
 	}
 
 	return (
 		<div>
-		<div className="new_filter_container">
-			{selectedlist.length ?
-				<div className='selected_btn_prop'>
-					<div className='filter_apply_dlt'>
-						{/* <button className='clear_filter_btn' onClick={handlefilterapply}>Apply</button> */}
-						{/* <button className='clear_filter_btn' onClick={ClearAll}>Clear All</button> */}
+			<div className="new_filter_container">
+				{selectedlist.length ?
+					<div className='selected_btn_prop'>
+						<div className='filter_apply_dlt'>
+							{/* <button className='clear_filter_btn' onClick={handlefilterapply}>Apply</button> */}
+							{/* <button className='clear_filter_btn' onClick={ClearAll}>Clear All</button> */}
+						</div>
 					</div>
-				</div>
-				: ""
-			}
-			<div className="">
-				<div className="filter-col1 p-r-15 p-b-10 ">
-					<div>
-						{/* <p>Lorem, ipsum dolor.</p> */}
-						<h3 className="productheading ">{categoryheading ? "Product Categories" : "Sub Category"}</h3>
-					</div>
-					<ul className='productList'>
-						{categoryList.map((item, index) => {
-							return (
-								<div key={index}>
-									<li className="p-b-6">
-										{item.category_name === namevalue ?
-											<button onClick={item.selected ? () => onlistafterClick(item, categoryList, index) : () => onlistClick(item, categoryList, index)} className="filter-link stext-106 trans-04 list-filter clgray activeFilter">
-												{item.category_name}
-											</button>
-											:
-											<button onClick={item.selected ? () => onlistafterClick(item, categoryList, index) : () => onlistClick(item, categoryList, index)} className={`filter-link stext-106 trans-04 list-filter clgray ${item.selected ? "activeFilter" : ""}`}>
-												{item.category_name}
-											</button>
-										}
-									</li>
-								</div>
-							)
-						})}
-					</ul>
-				</div>
-
-				<div className="filter-col2 p-r-15 p-b-10">
-					<div className="priceheading">
-						Filter By Price
+					: ""
+				}
+				<div className="">
+					<div className="filter-col1 p-r-15 p-b-10 ">
+						<div>
+							{/* <p>Lorem, ipsum dolor.</p> */}
+							<h3 className="productheading ">{categoryheading ? "Product Categories" : "Sub Category"}</h3>
+						</div>
+						<ul className='productList'>
+							{categoryList.map((item, index) => {
+								return (
+									<div key={index}>
+										<li className="p-b-6">
+											{item.category_name === namevalue ?
+												<button onClick={item.selected ? () => onlistafterClick(item, categoryList, index) : () => onlistClick(item, categoryList, index)} className="filter-link stext-106 trans-04 list-filter clgray activeFilter">
+													{item.category_name}
+												</button>
+												:
+												<button onClick={item.selected ? () => onlistafterClick(item, categoryList, index) : () => onlistClick(item, categoryList, index)} className={`filter-link stext-106 trans-04 list-filter clgray ${item.selected ? "activeFilter" : ""}`}>
+													{item.category_name}
+												</button>
+											}
+										</li>
+									</div>
+								)
+							})}
+						</ul>
 					</div>
 
-					<div className='price_slider'>
-						<Slider
-							getAriaLabel={() => 'Minimum distance'}
-							value={range}
-							onChange={handleChange}
-							valueLabelDisplay="auto"
-							step={250}
-							min={0}
-							max={10000}
-							disableSwap
-						/>
-					</div>
-					<div className='input_container_filter mt-3'>
-						<div className='price_lable_input'
-						style={{
-							
-						}}>
-							<p>From</p>
-							<input type="number" value={minPrice} onChange={handleMinPriceChange} className='price_input mx-2' 
-							style={{
-							
-							width:"80px",
-							border:"1px solid var(--secondary-color) !important",
-							paddingLeft:"3px"
-						}}	
+					<div className="filter-col2 p-r-15 p-b-10">
+						<div className="priceheading">
+							Filter By Price
+						</div>
+
+						<div className='price_slider'>
+							<Slider
+								getAriaLabel={() => 'Minimum distance'}
+								value={range}
+								onChange={handleChange}
+								valueLabelDisplay="auto"
+								step={250}
+								min={0}
+								max={10000}
+								disableSwap
 							/>
 						</div>
 
-						<div className='price_lable_input'
-						style={{
-							
-						}}>
-							<p style={{ marginLeft: "10px" }}>To</p>
-							<input type="number" value={maxPrice} onChange={handleMaxPriceChange} className='price_input mx-2' 
-							style={{
-							
-							width:"80px",
-							border:"1px solid var(--secondary-color) !important",
-							paddingLeft:"3px"
-						}}		
+						<div className="d-flex">
+
+							<TextField
+								value={minPrice}
+								onChange={handleMinPriceChange}
+								size="small"
+								InputProps={{
+									startAdornment: (
+										<InputAdornment position="start">
+											From Rs
+										</InputAdornment>
+									),
+								}}
+								sx={{
+									width: '150px',
+									'& .MuiOutlinedInput-root': {
+										'& fieldset': {
+											border: 'none',
+										},
+									},
+								}}
+							/>
+
+							<TextField
+								value={maxPrice}
+								onChange={handleMaxPriceChange}
+								size="small"
+								InputProps={{
+									startAdornment: (
+										<InputAdornment position="start">
+											To Rs
+										</InputAdornment>
+									),
+								}}
+								sx={{
+									width: '150px',
+									'& .MuiOutlinedInput-root': {
+										'& fieldset': {
+											border: 'none',
+										},
+									},
+								}}
 							/>
 						</div>
+
+
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 	)
 }
 
